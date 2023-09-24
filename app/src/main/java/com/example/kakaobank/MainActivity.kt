cf396d4c300e7a1b8673323ce302a6876ec42adf
Lee Sang Hoon <139089046+2020304056@users.noreply.github.com>
2023-09-24 12:29:47 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/kakaobank/MainActivity.kt
+++ app/src/main/java/com/example/kakaobank/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.kakaobank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kakaobank.MainViewPagerAdapter
+import com.example.kakaobank.ImageDataModel
+import com.example.kakaobank.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewPagerAdapter by lazy {
+        MainViewPagerAdapter(this)
+    }
+
+    private val tabIcon = arrayListOf(
+        R.drawable.home_size,
+        R.drawable.lockers,
+    )
+
+    var likedItems: ArrayList<ImageDataModel> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+
     }
+
+    private fun initView() = with(binding){
+        viewpager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewpager) { tab, position ->
+            tab.setText(viewPagerAdapter.getTitle(position))
+            tab.setIcon(tabIcon[position])
+        }.attach()
+    }
+
+
+
+
 }
